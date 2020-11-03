4a60acc13569d76580ba552206c126ac9fb969db
QusstW <den19081997@mail.ru>
2020-11-04 01:14:05 +0300

    Update structure and add massive in dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,11 +1,65 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from "./Dialogs.module.css"
 
 
-const Dialogs = (props) =>{
+const DialogItem = (props) => {
+    return (
+        <div className={s.dialog + " " + s.active}>
+            <NavLink to={"/Dialogs/" + props.id}> {props.name} </NavLink>
+        </div>
 
-    return(
-        <div>Dialogs</div>
+    )
+}
+
+const Message = (props) => {
+    return (
+        <div>
+            <div className={s.message}>{props.message}</div>
+        </div>
+
+    )
+}
+
+
+const Dialogs = (props) => {
+
+    let dialogaData =[
+        {id: 1, name:"Антоха"},
+        {id: 2, name:"Илюха"},
+        {id: 3, name:"Сеня"},
+        {id: 4, name:"Гена"},
+        {id: 5, name:"Санёк"},
+        {id: 6, name:"Данёк"},
+ 
+ ]
+
+    let messageData=[
+        {id: 1, message:"Привет!"},
+        {id: 2, message:"Как дела?"},
+        {id: 3, message:"Что нового?"},
+
+    ]
+
+    return (
+        <div className={s.dialogsList}>
+            <div className={s.dialogs_items}>
+                <DialogItem name={dialogaData[0].name} id={dialogaData[0].id} />
+                <DialogItem name={dialogaData[1].name} id={dialogaData[1].id} />
+                <DialogItem name={dialogaData[2].name} id={dialogaData[2].id} />
+                <DialogItem name={dialogaData[3].name} id={dialogaData[3].id} />
+                <DialogItem name={dialogaData[4].name} id={dialogaData[4].id} />
+                <DialogItem name={dialogaData[5].name} id={dialogaData[5].id} />
+            </div>
+
+            <div>
+                <Message message={messageData[0].message} />
+                <Message message={messageData[1].message} />
+                <Message message={messageData[2].message} />
+
+            </div>
+        </div>
+
     )
 }
 
--- src/components/ProfileComponent/AvaAndDescription/AvaAndDescription.jsx
+++ src/components/ProfileComponent/AvaAndDescription/AvaAndDescription.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import s from "./AvaAndDescription.module.css";
 
+
+
+
+const ProfileInfo=(props)=>{
+  return(
+    <div className={s.AvaAndDescription}>
+    <div className={s.AVA}>
+      <img src="https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg" />
+    </div>
+    <div>Descriptoin</div>
+  </div>
+  )
+}
+
+
 const AvaAndDescription = () => {
   return (
-      <div className={s.AvaAndDescription}>
-        <div className={s.AVA}>
-          <img src="https://i.pinimg.com/originals/33/b8/69/33b869f90619e81763dbf1fccc896d8d.jpg" />
-        </div>
-        <div>Descriptoin</div>
-      </div>
+    <ProfileInfo />
   )
 }
 
